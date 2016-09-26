b33d39e51a4748e2c3145f0519f93d9fdde79cda
Tom Scott <tomscott85@gmail.com>
2016-09-26 21:01:56 +1000

    complete the handwritingClassTest function

--- knn.py
+++ knn.py
@@ -89,7 +89,7 @@ def img2vector(filename):
 
 
 def handwritingClassTest():
-	hwLabels =[]
+	hwLabels = []
 	trainingFileList = listdir('trainingDigits')
 	m = len(trainingFileList)
 	trainingMat = zeros((m,1024))
@@ -99,10 +99,21 @@ def handwritingClassTest():
 		classNumStr = int(fileStr.split('_')[0])
 		hwLabels.append(classNumStr)
 		trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
-		testFileList = listdir('testDigits')
-
-
-
+	testFileList = listdir('testDigits')
+	errorCount = 0.0
+	mTest = len(testFileList)
+	for i in range(mTest):
+		fileNameStr = testFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+		classifierResult = classify0(vectorUnderTest, \
+			trainingMat, hwLabels, 3)
+		print "the classifier came back with: %s, the real answer is: %s"\
+			% (classifierResult, classNumStr)
+		if (classifierResult != classNumStr): errorCount += 1.0
+	print "\nthe total number of errors is: %d" % errorCount
+	print "\nthe total error rate is: %f" % (errorCount/float(mTest))
 
 
 
